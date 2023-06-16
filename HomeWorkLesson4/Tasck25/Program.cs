ea16d263a86902c095cbc923be85ed1adf15fcd5
EdvardEgorov <135224482+EdvardEgorov@users.noreply.github.com>
2023-06-16 11:03:21 +0300

    Add files via upload

---
+++ HomeWorkLesson4/Tasck25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+for (int i = 1; i < b; i++)
+{
+step = step * a;
+}
+Console.WriteLine($" Результат: " + step);
